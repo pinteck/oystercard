3e6972a598f10091ddecb94e285310f727ba8f3f
pinar <pnr_ozbakir@msn.com>
2021-09-27 22:02:32 +0100

    add context & before block

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,27 +6,26 @@ describe Oystercard do
     it 'has an initial balance' do
       expect(subject.balance).to eq 0 
     end
-  end
 
-  describe '#top_up' do
     it 'adds money to the card' do
       expect { subject.top_up 1 } .to change { subject.balance }.by 1
     end
 
-    it 'raises and error if the new balance would exceed the limit' do
-      max_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(max_balance)
-      expect{subject.top_up 1 }.to raise_error "Maximum limit of #{max_balance} exceeded"
-    end
-  end
-
-  describe '#deduct' do
     it 'deducts the fare value from the balance' do
       expect {subject.deduct 1 } .to change { subject.balance }.by -1
     end
   end
+  
+  context 'when the card is full' do
+    before do
+      subject.top_up(Oystercard::MAXIMUM_BALANCE)
+    end
 
-  describe '#status' do
+    it 'raises and error if the new balance would exceed the limit' do
+      max_balance = Oystercard::MAXIMUM_BALANCE
+      expect{subject.top_up 1 }.to raise_error "Maximum limit of #{max_balance} exceeded"
+    end
+
     it { is_expected.not_to be_in_journey }
     it 'changes the status to be in journey after touching in' do
       subject.touch_in
