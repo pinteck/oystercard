cd495fb4f60614cfc63735f0e057636635d06275
pinar <pnr_ozbakir@msn.com>
2021-09-27 15:19:18 +0100

    Ch:5 Refactor code

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,9 +5,12 @@ describe Oystercard do
     expect(subject.balance).to eq 0 
   end
 
-  it 'adds money to the card' do
-    my_oyster = Oystercard.new
-    my_oyster.top_up(10)
-    expect(my_oyster.balance).to eq 10
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'adds money to the card' do
+      expect { subject.top_up 1 } .to change { subject.balance }.by 1
+    end
   end
+
 end
