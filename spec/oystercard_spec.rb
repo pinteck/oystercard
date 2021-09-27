e05ecdc6502ab962c4b7e74a68f64ebc52023f44
pinar <pnr_ozbakir@msn.com>
2021-09-27 15:16:12 +0100

    Ch5: add top-up feature

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,10 @@ describe Oystercard do
   it 'has an initial balance' do
     expect(subject.balance).to eq 0 
   end
+
+  it 'adds money to the card' do
+    my_oyster = Oystercard.new
+    my_oyster.top_up(10)
+    expect(my_oyster.balance).to eq 10
+  end
 end
